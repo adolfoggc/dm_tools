1cd623575e3a40a6fbfc2420c8dbe6e1737ce21f
Carlos Adolfo <adolfo.ggc@gmail.com>
2023-04-14 19:25:47 -0300

    Add dashboard#encounter_calculator

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  resources :monsters
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
-
-
+  
+  
   authenticate :user do
     root 'dashboard#home'
   end
-
+  
   authenticated :user do
+    resources :monsters
+
     get '/', to: 'dashboard#home', as: 'auth_home'
+
+    get '/encounter_calculator', to: 'dashboard#encounter_calculator', as: 'encounter_calculator'
   end
 
 end
