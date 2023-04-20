5aa5688cf66a9f093788d106c035387cdce67483
Carlos Adolfo <adolfo.ggc@gmail.com>
2023-04-20 10:28:51 -0300

    Update combat equipment model

--- app/models/combat_equipment.rb
+++ app/models/combat_equipment.rb
@@ -16,8 +16,34 @@ class CombatEquipment < ApplicationRecord
     if I18n.locale.to_s == 'en'
       weight + 'lb'.pluralize(weight)
     else
-      converted_weight = weight/2
+      converted_weight = (weight/2).to_i
       converted_weight + 'kg'.pluralize(converted_weight)
     end
   end
+end
+
+def donning_armor
+  case kind
+  when 'light_armor'
+    '1 ' + t('general.minute')
+  when 'medium_armor'
+    '5 ' + t('general.minute').pluralize
+  when 'heavy_armor'
+    '10 ' + t('general.minute').pluralize
+  else
+    '1 ' + t('general.action')
+  end 
+end
+
+def doffing_armor
+  case kind
+  when 'light_armor'
+    '1 ' + t('general.minute')
+  when 'medium_armor'
+    '1 ' + t('general.minute')
+  when 'heavy_armor'
+    '5 ' + t('general.minute').pluralize
+  else
+    '1 ' + t('general.action')
+  end 
 end
