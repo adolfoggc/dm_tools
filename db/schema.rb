decfbc9392260e3cce4bbd1938f87a8023385909
Carlos Adolfo <adolfo.ggc@gmail.com>
2023-04-20 22:32:32 -0300

    Update migration and schema for weapon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_204305) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_005145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_204305) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "weapons", force: :cascade do |t|
+    t.string "name"
+    t.integer "kind"
+    t.integer "melee_or_ranged"
+    t.boolean "one_handed", default: true
+    t.string "dmg"
+    t.integer "price"
+    t.integer "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_weapons_on_name", unique: true
   end
 
 end
