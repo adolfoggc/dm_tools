ed6daa7c681727e7cc3399754aa24ccd61e296b1
Carlos Adolfo <adolfo.ggc@gmail.com>
2023-04-28 08:13:00 -0300

    Add check on encounter calculator to get the translation and order the result

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,13 @@ class DashboardController < ApplicationController
   def encounter_calculator
     @monsters = {}
     Monster.all.order(:name).select('name', 'challange_rating').each do |m|
-      @monsters[m.name.humanize.capitalize] = m.challange_rating
+      if I18n.exists?(m.name, :pt_BR)
+        name = t(m.name)
+      else
+        name = m.name.humanize.capitalize
+      end
+      @monsters[name] = m.challange_rating
     end
+    @monsters = @monsters.sort.to_h
   end
 end
