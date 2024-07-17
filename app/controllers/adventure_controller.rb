fb3d63c65cd80dfdfd941521d39965e3ac8524b3
Carlos Adolfo <adolfo.ggc@gmail.com>
2024-07-16 23:26:46 -0300

    Update combat view

--- app/controllers/adventure_controller.rb
+++ app/controllers/adventure_controller.rb
@@ -1,6 +1,5 @@
 class AdventureController < ApplicationController
   def combat
-    @char_names = Sheet.all.pluck(:name, :id).to_h
-    @char_names2 = Sheet.all.pluck(:name, :id)
+    @char_names = Sheet.all.pluck(:name, :id)
   end
 end
